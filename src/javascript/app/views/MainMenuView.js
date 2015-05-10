cdeae1e88ea3b004459f2704870b3eb04e8b25b0
caleb jeffrey <calebjeffrey.dev@gmail.com>
2015-05-10 15:44:38 -0700

    added basemodalview and hamburger

--- src/javascript/app/views/MainMenuView.js
+++ src/javascript/app/views/MainMenuView.js
@@ -1,18 +1,40 @@
 var BaseView = require('views/BaseView');
 var template = require('templates/main-menu.hbs');
+var constants = require('utils/constants');
 
 module.exports = BaseView.extend({
 
     template: template,
 
-    templateHelpers: function() {
+    ui: {
+        hamburger: '.hamburger',
+        toggleMenu: '.toggle-menu'
+    },
+
+    events: {
+        'click @ui.toggleMenu': 'onClickHamburger'
     },
 
     initialize: function(options) {
         console.log(options);
+        this.menuOpen = false;
     },
 
     onShow: function() {
-    }
+    },
+
+    onClickHamburger: function () {
+        this.toggleMenu();
+    },
+
+    toggleMenu: function () {
+        this.menuOpen = !this.menuOpen;
+
+        if (this.menuOpen) {
+            this.ui.hamburger.addClass(constants.MENU_OPEN_CLASS);
+        } else {
+            this.ui.hamburger.removeClass(constants.MENU_OPEN_CLASS);
+        }
+    },
 
 });
