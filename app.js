7f44a19714cf3c4f0f41113fcf63f0b736e4cb34
caleb jeffrey <calebjeffrey.dev@gmail.com>
2015-05-10 15:22:23 -0700

    app.js skeleton

---
+++ app.js
@@ -0,0 +1,37 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express'),
+    methodOverride = require('method-override'),
+    path = require('path'),
+    bodyParser = require('body-parser');
+
+// create express instance
+var app = express();
+
+// static folder setup
+app.use(express.static(path.join(__dirname, './static/')));
+
+// initialize authentication
+// cors
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+
+    next();
+});
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+app.get('*', function(req, res){
+    res.sendFile(__dirname + '/build/index.html');
+});
+
+// listen
+app.set('port', process.env.PORT || 8000);
+app.listen(app.get('port'));
