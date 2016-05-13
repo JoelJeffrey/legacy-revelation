c8912554d7df84e0fa5b42b2c015d460c3c63fa1
Joel Jeffrey <joel.jeffrey@sprinklr.com>
2016-05-13 12:00:05 -0500

    Updated chronicle information, need to format the json stories to wrap in <p> tags

--- src/javascript/app/data/storyData.js
+++ src/javascript/app/data/storyData.js
@@ -1,20 +1,64 @@
 module.exports = [
     {
-        "slug": "story-1",
-        "title": "Story-1",
-        "content": "this is a story, holy smokes it's killer!",
-        "img": "xakovi-thumb.png"
+        "slug": "lerykaz-1",
+        "title": "Lerykaz",
+        "story": "<p>Starlight filtered through the dark canopy of a forest on Xydak. Pools of blue-tinged light lit Lerykaz’ way as he shambled through the undergrowth, nearly tripping on thick roots that rose from the soil. Arms limp, eyes unfocused and crusted with dried tears, he shuffled on through the darkness. Putting one foot in front of the other took more effort than it ever should.</p>\
+            <p>Lerykaz finally tripped, sending him tumbling forward over something hard and cool in the night air. Tracing the rough, furrowed surface with his hands Lerykaz could feel the material flaking off against his fingers. It was bark. Tracing the uneven surface, following the ghost of his hand in the starlight, he realized it was the base of a massive tree. The trunk rose into the night sky, vanishing into the darkness of space, its leaves blotting out the stars and merging with the void.</p>\
+                <p>There seemed to be a narrow gap between the root and its massive brother. Shouldering his knapsack, Lerykaz carefully rolled over the root and nestled himself into the tree’s base. There was still a trace of warmth left from the Orb’s touch during the day and he found himself settling further down into the ground to feel it.</p>\
+                <p>How long he slumped there staring at sparkling stars, dark leaves, and space beyond he could never say. He could only remember the sensation of being wedged in those tree roots, seemingly able to feel the ancient, thick foundation working its way deeper into the ground. They pulsed with life, he discovered, humming against his skin. The entire forest was alive. He had simply merged with the natural rhythm of the night: a gentle cool breeze that caressed his face and rustled the leaves in time with his breathing.</p>\
+                <p>Though distant, the sharp cries of yvichu at hunt matched the pinpricks of pain across his body. Blood, once tacky, was beginning to itch as it caked dry under his armor and clothes.</p>\
+                <p>Even now, hours removed from the battle — It had been just this morning, hadn’t it? — he couldn’t remember exactly how it happened. The smell of dew was still in his nose, though it could have been the mustier, heavier scent that surrounded him in the tree’s base. Did it matter? He could remember moving through the forest, greaves shining from the dew and morning light. The tower had risen from the horizon perfectly straight, dominating the landscape.</p>\
+                <p>He made it past the traps, the snares, the defenses he’d expected and those he hadn’t. He went up a staircase that never seemed to end, passed rooms where things hissed, groaned, and shrieked at his footsteps. None of that mattered. Not now. The fight itself had been vicious; the scorch marks across his chest, the rips in his armor, the thin slices across his forearms that seemed darker than the night sky, were all testament to that.</p>\
+                <p>Lerykaz jolted upright, hand grasping at what was an empty hilt. His sword. He’d left it behind. He’d left it buried in his father’s back.</p>\
+                <p>His father. He hadn’t known it then. Not until he rolled the man over and pulled back the hood. Too late now. Too late to do anything about that.</p>\
+                <p>Rough bark scratched at his face as Lerykaz slumped against the tree. His eyes still refused to slide shut no matter how much his body pleaded for rest. Overhead the canopy of branches creaked and their leaves shuffled as the wind picked up. Pools of blue star light danced and merged with the shifting shadows and the shapes seemed to worm their way behind Lerykaz’ eyes. He could feel the shapes touching his mind, filling the void left behind as memory and thought drained away.</p>\
+                <p>His tears had run out hours ago.</p>\
+                <p>Ever so slowly his muscles began to uncoil. His limbs, so heavy this day, began to relax, seemingly falling away. Finally his eyes began to blink, to droop. As they slid shut he could still see the swirling stars and shadows in the darkness behind his lids. They had truly merged with his mind. It wasn’t such a bad thing, he decided, before giving into unconsciousness.</p>\
+                <p>As Lerykaz gave into exhaustion his subconsciousness still remembered one thing: protect what you have saved. His arms and legs curled around his battered knapsack. Though thick and sturdy, the glow of forty vortexes still leaked out through a threadbare seam near the strap. All his father’s research sat there, waiting patiently for Lerykaz to awake and discover his inheritance.</p>\
+                <p>Yet nestled among the strands of vortexes was something even more precious. His eyes tightly shut against the vortexes’ glow, Karzyrel slept in his makeshift cradle. The infant’s chest rose and fell in perfect time with his older brother’s as the forest lulled them into deeper sleep.</p>",
+        "img": "../chronicle/L-pic.jpg"
     },
     {
-        "slug": "story-2",
-        "title": "Story-2",
-        "content": "this is a story, holy smokes it's killer!",
-        "img": "xakovi-thumb.png"
+        "slug": "lerykaz-2",
+        "title": "Lerykaz",
+        "story": "this is a story, holy smokes it's killer!",
+        "img": "../chronicle/L-pic.jpg"
     },
     {
-        "slug": "story-3",
-        "title": "Story-3",
-        "content": "this is a story, holy smokes it's killer!",
-        "img": "xakovi-thumb.png"
+        "slug": "lerykaz-3",
+        "title": "Lerykaz",
+        "story": "this is a story, holy smokes it's killer!",
+        "img": "../chronicle/L-pic.jpg"
+    },
+    {
+        "slug": "methkog-1",
+        "title": "Methkog",
+        "story": "Starlight filtered through the dark canopy of a forest on Xydak. Pools of blue-tinged light lit Lerykaz’ way as he shambled through the undergrowth, nearly tripping on thick roots that rose from the soil. Arms limp, eyes unfocused and crusted with dried tears, he shuffled on through the darkness. Putting one foot in front of the other took more effort than it ever should.\
+                <p>Lerykaz finally tripped, sending him tumbling forward over something hard and cool in the night air. Tracing the rough, furrowed surface with his hands Lerykaz could feel the material flaking off against his fingers. It was bark. Tracing the uneven surface, following the ghost of his hand in the starlight, he realized it was the base of a massive tree. The trunk rose into the night sky, vanishing into the darkness of space, its leaves blotting out the stars and merging with the void.</p>\
+                <p>There seemed to be a narrow gap between the root and its massive brother. Shouldering his knapsack, Lerykaz carefully rolled over the root and nestled himself into the tree’s base. There was still a trace of warmth left from the Orb’s touch during the day and he found himself settling further down into the ground to feel it.</p>\
+                <p>How long he slumped there staring at sparkling stars, dark leaves, and space beyond he could never say. He could only remember the sensation of being wedged in those tree roots, seemingly able to feel the ancient, thick foundation working its way deeper into the ground. They pulsed with life, he discovered, humming against his skin. The entire forest was alive. He had simply merged with the natural rhythm of the night: a gentle cool breeze that caressed his face and rustled the leaves in time with his breathing.</p>\
+                <p>Though distant, the sharp cries of yvichu at hunt matched the pinpricks of pain across his body. Blood, once tacky, was beginning to itch as it caked dry under his armor and clothes.</p>\
+                <p>Even now, hours removed from the battle — It had been just this morning, hadn’t it? — he couldn’t remember exactly how it happened. The smell of dew was still in his nose, though it could have been the mustier, heavier scent that surrounded him in the tree’s base. Did it matter? He could remember moving through the forest, greaves shining from the dew and morning light. The tower had risen from the horizon perfectly straight, dominating the landscape.</p>\
+                <p>He made it past the traps, the snares, the defenses he’d expected and those he hadn’t. He went up a staircase that never seemed to end, passed rooms where things hissed, groaned, and shrieked at his footsteps. None of that mattered. Not now. The fight itself had been vicious; the scorch marks across his chest, the rips in his armor, the thin slices across his forearms that seemed darker than the night sky, were all testament to that.</p>\
+                <p>Lerykaz jolted upright, hand grasping at what was an empty hilt. His sword. He’d left it behind. He’d left it buried in his father’s back.</p>\
+                <p>His father. He hadn’t known it then. Not until he rolled the man over and pulled back the hood. Too late now. Too late to do anything about that.</p>\
+                <p>Rough bark scratched at his face as Lerykaz slumped against the tree. His eyes still refused to slide shut no matter how much his body pleaded for rest. Overhead the canopy of branches creaked and their leaves shuffled as the wind picked up. Pools of blue star light danced and merged with the shifting shadows and the shapes seemed to worm their way behind Lerykaz’ eyes. He could feel the shapes touching his mind, filling the void left behind as memory and thought drained away.</p>\
+                <p>His tears had run out hours ago.</p>\
+                <p>Ever so slowly his muscles began to uncoil. His limbs, so heavy this day, began to relax, seemingly falling away. Finally his eyes began to blink, to droop. As they slid shut he could still see the swirling stars and shadows in the darkness behind his lids. They had truly merged with his mind. It wasn’t such a bad thing, he decided, before giving into unconsciousness.</p>\
+                <p>As Lerykaz gave into exhaustion his subconsciousness still remembered one thing: protect what you have saved. His arms and legs curled around his battered knapsack. Though thick and sturdy, the glow of forty vortexes still leaked out through a threadbare seam near the strap. All his father’s research sat there, waiting patiently for Lerykaz to awake and discover his inheritance.</p>\
+                <p>Yet nestled among the strands of vortexes was something even more precious. His eyes tightly shut against the vortexes’ glow, Karzyrel slept in his makeshift cradle. The infant’s chest rose and fell in perfect time with his older brother’s as the forest lulled them into deeper sleep.</p>",
+        "img": "../chronicle/M-pic.jpg"
+    },
+    {
+        "slug": "methkog-2",
+        "title": "Methkog",
+        "story": "this is a story, holy smokes it's killer!",
+        "img": "../chronicle/M-pic.jpg"
+    },
+    {
+        "slug": "methkog-3",
+        "title": "Methkog",
+        "story": "this is a story, holy smokes it's killer!",
+        "img": "../chronicle/M-pic.jpg"
     }
 ];
