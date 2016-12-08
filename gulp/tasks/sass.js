68c3e1fbd8d99247727fcd5ad1342ceb9aa20427
Joel Jeffrey <JoelJeffrey.dev@gmail.com>
2016-12-08 03:15:06 -0600

    had to remove sass sourcemaps, which was breaking the build.  Also updated some copy for the 'Updates Modal'

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -8,10 +8,8 @@ var autoprefixer = require('gulp-autoprefixer');
 
 var taskDef = function () {
     return gulp.src(config.src)
-        .pipe(sourcemaps.init())
         .pipe(sass(config.settings))
         .on('error', handleErrors)
-        .pipe(sourcemaps.write())
         .pipe(autoprefixer({
             browsers: ['last 2 versions']
         }))
